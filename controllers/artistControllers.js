5b1039baf9578540a7bce18271bb148fb51a73fc
FeeMowa <lyonmowa@gmail.com>
2022-12-13 11:39:07 +0100

    converted to csv file

---
+++ controllers/artistControllers.js
@@ -0,0 +1,63 @@
+import axios from "axios";
+import "dotenv/config";
+import colors from "colors";
+import converter from "json-2-csv";
+import fs from "fs";
+import { URL } from "url";
+
+const __dirname = decodeURI(new URL("..", import.meta.url).pathname);
+const FolderPath = __dirname;
+
+// ==>> Task-1 ==>> Search for an artist by name based on the following endpoint artist.search, return all the results for this artist.
+// http://ws.audioscrobbler.com/2.0/?method=artist.search&artist=cher&api_key=YOUR_API_KEY&format=json
+
+const getArtistByName = async (req, res) => {
+  try {
+    const searchParams = await req.params.search;
+    const url = `https://ws.audioscrobbler.com/2.0/?method=artist.search&artist=${searchParams}&api_key=${process.env.API_KEY}&format=json`;
+    const response = await axios(url);
+    // receive data for matching artists
+    const data = await response.data;
+    // console.log(data);
+    res.send(data);
+    //specifying the search results in the data received.
+    const artistsArray = await data.results.artistmatches.artist;
+    // console.log(artistsArray);
+
+    // ==> Task-2 ==> Write the result to a user-supplied CSV filename.The CSV file should include the following information (name, mbid, url, image_small image)
+    const artistsData = [];
+    if (artistsArray.length > 0) {
+      for (const artist of artistsArray) {
+        const image_small = Object.values(artist.image[0]);
+        const image = Object.values(artist.image[2]);
+
+        artistsData.push({
+          artist: artist.name,
+          mbid: artist.mbid,
+          url: artist.url,
+          image_small: `${image_small}`,
+          image: `${image}`,
+        });
+        // console.log(artistsData);
+      }
+      converter.json2csv(artistsData, (err, csv) => {
+        if (err) {
+          throw err;
+        }
+        // console.log(csv);
+        // for writing all the data to the one CSV file use:
+        // fs.writeFileSync(`${FolderPath}/search/artistsArray.csv`, csv);
+        fs.writeFileSync(`${FolderPath}/search/${searchParams}.csv`, csv);
+        console.log(
+          "csv file has been written successfully into Search Folder.".bgGreen
+        );
+      });
+      // ==>> Task-3==>> If no results returned from the artist.search endpoint, retrieve random artist names from a JSON dictionary source
+    } else {
+    }
+  } catch (error) {
+    console.log(error);
+    res.json(error);
+  }
+};
+export { getArtistByName };
--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 import express from "express";
 import "dotenv/config";
+import colors from "colors";
+import { getArtistByName } from "./controllers/artistControllers.js";
+
 // starting the express server
 const app = express();
 const PORT = process.env.PORT || 3002;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/artist/:search", getArtistByName);
+
 app.listen(PORT, () => {
-  console.log(`Connected to port ${PORT}`);
+  console.log(`Connected to port ${PORT}....`.bgMagenta);
 });
